61fe59a682794042b768810f27413e7ec86eef66
Морозов Павел Павлович <fess-master@yandex.ru>
2015-11-16 23:42:16 +0300

    bellman alg restructure

---
+++ NP/Algs/BellmanAlg.cs
@@ -0,0 +1,44 @@
+﻿using Algs;
+
+
+namespace ExistingAlgs
+{
+    public class BellmanAlg
+    {
+        private readonly int _maxWeight;
+        private readonly int _goodsCount;
+        private static int[,] _matrix; //массив для хранения значений функции
+
+
+        public BellmanAlg(int maxWeight, int goodsCount)
+        {
+            _maxWeight = maxWeight;
+            _goodsCount = goodsCount;
+        }
+
+        public void Calc(Good[] goods)
+        {
+            _matrix = new int[_maxWeight + 1, _goodsCount]; //Реализуем массив функции
+
+            //Реализуем алгоритм Беллмана
+            for (int weight = 1; weight <= _maxWeight; weight++) // Загружаем рюкзак если его вместимость = Weight
+                for (int i = 1; i < _goodsCount; i++) // берем предметы с 1 по goodsCount
+                    //если вес предмета больше Weight, или предыдущий набор лучше выбираемого
+                    if (goods[i].Weight > weight)
+                    {
+                        _matrix[weight, i] = _matrix[weight, i - 1]; //тогда берем предыдущий набор
+                        goods[i].Take = false;
+                    }
+                    else if (_matrix[weight, i - 1] >= (_matrix[weight - goods[i].Weight, i - 1] + goods[i].Price))
+                    {
+                        _matrix[weight, i] = _matrix[weight, i - 1]; //тогда берем предыдущий набор
+                        goods[i].Take = false;
+                    }
+                    else
+                    {
+                        _matrix[weight, i] = _matrix[weight - goods[i].Weight, i - 1] + goods[i].Price; //иначе добавляем к предыдущему набору текущий предмет
+                        goods[i].Take = true;
+                    }
+        }
+    }
+}
--- NP/ExistingAlgs/Program.cs
+++ NP/ExistingAlgs/Program.cs
@@ -1,15 +1,106 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Algs;
+using ExistingAlgs;
 
-namespace ExistingAlgs
+namespace ryukzak
 {
     class Program
     {
+        private static int _goodsCount; // Количество товаров
+        private static bool incorrEnter; //Переменная, что проверяет корректность ввода
+        private static string _enteredName; // Имя товара
+        private static int _enteredWeight, _enteredPrice, _bagMaxWeight; // Вес и цена товара, размер рюкзака
+
+
+        // Метод выводит на экран рюкзак
+        static void Print(Good[] goods)
+        {
+            Console.WriteLine("\nМаксимальная стоимость: " + BellmanAlg.func[_bagMaxWeight, _goodsCount - 1]);
+            Console.Write("Взяты следующие предметы: ");
+            foreach (Good good in goods)
+                if (good.Take)
+                    Console.Write(good.Name + " ");
+        }
+
         static void Main(string[] args)
         {
+            int i, j; //просто переменные :)
+
+            Console.WriteLine("Данная программа поможет Вам заполнить Ваш рюкзак максимально ценными товарами\nАвтор: Владимир Рыбалка");
+
+            //Введем количество товаров
+            do
+            {
+                incorrEnter = false;
+                Console.Write("\nКакое количество товаров вы рассматриваете для приобретения: ");
+                try
+                {
+                    _goodsCount = Convert.ToInt32(Console.ReadLine());
+                }
+                catch (FormatException)
+                {
+                    incorrEnter = true;
+                    _goodsCount = 0;
+                }
+            } while (incorrEnter);
+
+            var goods = new Good[_goodsCount]; //Создаем массив заданного размера
+
+            //Создадим объекты товаров
+            for (i = 0, j = i + 1; i < goods.Length; i++, j++)
+            {
+                Console.Write("\n\nВведите название " + j + "-го товара: ");
+                _enteredName = Console.ReadLine();
+                do
+                {
+                    incorrEnter = false;
+                    Console.Write("Введите вес товара: ");
+                    try
+                    {
+                        _enteredWeight = Convert.ToInt32(Console.ReadLine());
+                    }
+                    catch (FormatException)
+                    {
+                        incorrEnter = true;
+                        _enteredWeight = 0;
+                    }
+                } while (incorrEnter);
+                do
+                {
+                    incorrEnter = false;
+                    Console.Write("Введите цену товара: ");
+                    try
+                    {
+                        _enteredPrice = Convert.ToInt32(Console.ReadLine());
+                    }
+                    catch (FormatException)
+                    {
+                        incorrEnter = true;
+                        _enteredPrice = 0;
+                    }
+                } while (incorrEnter);
+                goods[i] = new Good(_enteredName, _enteredWeight, _enteredPrice); //Создаем объект
+            }
+
+            // Введем размер рюкзака
+            do
+            {
+                incorrEnter = false;
+                Console.Write("\nВведите размер вашего рюкзака (контейнера): ");
+                try
+                {
+                    _bagMaxWeight = Convert.ToInt32(Console.ReadLine());
+                }
+                catch (FormatException)
+                {
+                    incorrEnter = true;
+                    _bagMaxWeight = 0;
+                }
+            } while (incorrEnter);
+
+            Print(goods);
+
+            Console.ReadKey();
         }
     }
 }
